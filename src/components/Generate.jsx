3a8a81381ede180424040729dced17daee1684a1
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-01-02 00:05:11 +0530

    Improve form

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Button, Form, Col } from 'react-bootstrap';
+import { Jumbotron, Button, Form } from 'react-bootstrap';
 
 class Generate extends React.Component {
   constructor(props) {
@@ -17,52 +17,34 @@ class Generate extends React.Component {
         <Form>
           <Form.Group>
             <Form.Label>Upload the CSV File</Form.Label>
-            <Form.Row align="center">
-              <Col>
-                <Form.File
-                  id="csvForm"
-                  accept=".csv"
-                  onChange={(e) => {
-                    if (e.target.files[0]) {
-                      this.setState({ uploadedCSV: true });
-                    } else {
-                      this.setState({ uploadedCSV: false });
-                    }
-                  }}
-                />
-              </Col>
-              <Col>
-                <Form.Check
-                  type="switch"
-                  disabled
-                  checked={this.state.uploadedCSV}
-                />
-              </Col>
+            <Form.Row>
+              <Form.File
+                id="csvForm"
+                accept=".csv"
+                onChange={(e) => {
+                  if (e.target.files[0]) {
+                    this.setState({ uploadedCSV: true });
+                  } else {
+                    this.setState({ uploadedCSV: false });
+                  }
+                }}
+              />
             </Form.Row>
           </Form.Group>
           <Form.Group>
             <Form.Label>Upload the Certificate Image</Form.Label>
-            <Form.Row align="center">
-              <Col>
-                <Form.File
-                  id="imgForm"
-                  accept="image/jpeg, image/png"
-                  onChange={(e) => {
-                    if (e.target.files[0]) {
-                      this.setState({ uploadedIMG: true });
-                    } else {
-                      this.setState({ uploadedIMG: false });
-                    }
-                  }}
-                />
-              </Col>
-              <Col>
-                <Form.Check
-                  type="switch"
-                  disabled
-                  checked={this.state.uploadedIMG}
-                />
-              </Col>
+            <Form.Row>
+              <Form.File
+                id="imgForm"
+                accept="image/jpeg, image/png"
+                onChange={(e) => {
+                  if (e.target.files[0]) {
+                    this.setState({ uploadedIMG: true });
+                  } else {
+                    this.setState({ uploadedIMG: false });
+                  }
+                }}
+              />
             </Form.Row>
           </Form.Group>
           <div align="center">
