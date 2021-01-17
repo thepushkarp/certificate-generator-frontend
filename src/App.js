b467ffe1f7adcf048942380bed85bf91e8ea8990
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-01-17 15:13:52 +0530

    Add media queries

--- src/App.js
+++ src/App.js
@@ -2,40 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Jumbotron } from 'react-bootstrap';
-import { UserAgentProvider, UserAgent } from '@quentin-sommer/react-useragent';
 import { Footer, Navigation, Home, Generate } from './components';
+import styles from './App.module.scss';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Navigation />
-
-        <UserAgentProvider ua={window.navigator.userAgent}>
-          <div>
-            <UserAgent mobile tablet>
-              <Jumbotron align="center">
-                <h1 className="display-4">Generate Certificates</h1>
-                <p className="lead">
-                  This is the certificate generation site for Indian Institute of
-                  Information Technology, Vadodara.
-                </p>
-                <p className="lead">
-                  You need to be on a computer to use this generator.
-                </p>
-              </Jumbotron>
-            </UserAgent>
-            <UserAgent computer>
-              <Router>
-                <Switch>
-                  <Route path="/" exact component={() => <Home />} />
-                  <Route path="/generate" exact component={() => <Generate />} />
-                </Switch>
-              </Router>
-            </UserAgent>
-          </div>
-        </UserAgentProvider>
-
+        <Jumbotron align="center" className={styles.contentMobile}>
+          <h1 className="display-4">Generate Certificates</h1>
+          <p className="lead">
+            This is the certificate generation site for Indian Institute of
+            Information Technology, Vadodara.
+          </p>
+          <p className="lead">You need to be on a computer to use this generator.</p>
+        </Jumbotron>
+        <div className={styles.contentDesktop}>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={() => <Home />} />
+              <Route path="/generate" exact component={() => <Generate />} />
+            </Switch>
+          </Router>
+        </div>
         <Footer />
       </>
     );
--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -5,63 +5,96 @@ class Generate extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploadedCSV: false,
-      uploadedIMG: false,
+      isCsvUploaded: false,
+      isImgUploaded: false,
+      isUploadButtonPressed: false,
+      certImage: null,
+      csvFile: null,
+      certWidth: 0,
+      certHeight: 0,
     };
   }
 
   render() {
     return (
-      <Jumbotron>
-        <h1 align="center">Upload the CSV and Certificate Image</h1>
-        <Form>
-          <Form.Group>
-            <Form.Label>Upload the CSV File</Form.Label>
-            <Form.Row>
-              <Form.File
-                id="csvForm"
-                accept=".csv"
-                onChange={(e) => {
-                  if (e.target.files[0]) {
-                    this.setState({ uploadedCSV: true });
-                  } else {
-                    this.setState({ uploadedCSV: false });
-                  }
-                }}
-              />
-            </Form.Row>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Upload the Certificate Image</Form.Label>
-            <Form.Row>
-              <Form.File
-                id="imgForm"
-                accept="image/jpeg, image/png"
-                onChange={(e) => {
-                  if (e.target.files[0]) {
-                    this.setState({ uploadedIMG: true });
-                  } else {
-                    this.setState({ uploadedIMG: false });
-                  }
-                }}
-              />
-            </Form.Row>
-          </Form.Group>
-          <div align="center">
-            <p>{`Uploaded ${
-              1 * this.state.uploadedCSV + 1 * this.state.uploadedIMG
-            }/2 files`}</p>
-            <Button
-              variant="primary"
-              size="lg"
-              type="submit"
-              disabled={!(this.state.uploadedCSV && this.state.uploadedIMG)}
-            >
-              Upload
-            </Button>
-          </div>
-        </Form>
-      </Jumbotron>
+      <>
+        {this.state.isUploadButtonPressed && (
+          <div
+            style={{
+              height: this.state.height,
+              weight: this.state.width,
+              backgroundImage: `url(${this.state.certImage})`,
+            }}
+          ></div>
+        )}
+        {!this.state.isUploadButtonPressed && (
+          <Jumbotron>
+            <h1 align="center">Upload the CSV and Certificate Image</h1>
+            <Form>
+              <Form.Group>
+                <Form.Label>Upload the CSV File</Form.Label>
+                <Form.Row>
+                  <Form.File
+                    id="csvForm"
+                    accept=".csv"
+                    onChange={(e) => {
+                      if (e.target.files[0]) {
+                        this.setState({ isCsvUploaded: true });
+                      } else {
+                        this.setState({ isCsvUploaded: false });
+                      }
+                    }}
+                  />
+                </Form.Row>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Upload the Certificate Image</Form.Label>
+                <Form.Row>
+                  <Form.File
+                    id="imgForm"
+                    accept="image/jpeg, image/png"
+                    onChange={(e) => {
+                      if (e.target.files[0]) {
+                        var imgUrl = URL.createObjectURL(e.target.files[0]);
+                        var img = new Image();
+                        img.src = imgUrl;
+                        img.onload = () => {
+                          this.setState({
+                            isImgUploaded: true,
+                            certImage: imgUrl,
+                            certWidth: img.width,
+                            certHeight: img.height,
+                          });
+                        };
+                      } else {
+                        this.setState({ isImgUploaded: false });
+                      }
+                    }}
+                  />
+                </Form.Row>
+              </Form.Group>
+              <div align="center">
+                <p>{`Uploaded ${
+                  1 * this.state.isCsvUploaded + 1 * this.state.isImgUploaded
+                }/2 files`}</p>
+                <Button
+                  variant="primary"
+                  size="lg"
+                  type="submit"
+                  disabled={!(this.state.isCsvUploaded && this.state.isImgUploaded)}
+                  onClick={(e) => {
+                    this.setState({
+                      isUploadButtonPressed: true,
+                    });
+                  }}
+                >
+                  Upload
+                </Button>
+              </div>
+            </Form>
+          </Jumbotron>
+        )}
+      </>
     );
   }
 }
