e7c767c6a9fa22a24f9b60e3d84b673b1b377601
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-02-05 02:31:23 +0530

    Fix link error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ class Footer extends React.Component {
       <footer className="footer-copyright text-center py-3">
         Made with &hearts; by&nbsp;
         <a
-          href="https:/github.com/iiitv/"
+          href="https://github.com/iiitv/"
           target="_blank"
           rel="noreferrer noopener"
         >
-          IIITV Coding Club;
+          IIITV Coding Club
         </a>
       </footer>
     );
